d29b8ce007c232d3698d5c624104bc0b51d5108d
soupman2 <jaysanau@gmail.com>
2026-02-16 20:00:51 +1100

    pyproject21

---
+++ main.py
@@ -0,0 +1,18 @@
+x=int(input("enter min range: "))
+y=int(input("enter max range: "))
+sqr=[]
+even=[]
+odd=[]
+for i in range(x,y+1):
+    i=i*i
+    sqr.append(i)
+ 
+print(f"squares '{x}' to '{y}' are '{sqr}'.")
+
+for j in sqr:
+    if j%2 == 0:
+        even.append(j)
+    else:
+        odd.append(j)
+
+print(f"even squares: {even}. odd squares: {odd}.")
